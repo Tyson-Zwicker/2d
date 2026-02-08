823e7739b75ffd8d72f9fb0a02934f966a044c63
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-08 01:17:52 -0400

    fixed delta calculation..

--- main.js
+++ main.js
@@ -20,22 +20,31 @@ export default class Main {
   static loop(startTime) {
     if (startTime) Main.oldTime = startTime;
     Main.time = Date.now();
-    Main.delta = Main.time - Main.oldTime;
-    if (Main.delta > Main.fpsMillis) console.log(`Main loop is taking to long: Delta: ${Main.delta} > ${Main.fpsMillis}`);
-    Main.oldTime = Main.time;    
+    Main.delta = (Main.time - Main.oldTime)/1000;;
+    //if (Main.delta > Main.fpsMillis) console.warn(`Main loop is taking to long: Delta: ${Main.delta} > ${Main.fpsMillis}`);
+    Main.oldTime = Main.time;
     Main.doWork();
     if (Main.continue) setTimeout(Main.loop, Main.fpsMillis);
-    console.log('exited.');
   }
   static doWork() {
     View.clear();
     Main.draw();
-    console.log ('draw complete '+Date.now());
-    Main.move();    
+    Main.move();
+    Main.checkMouse();
+    Main.showDelta();
+  }
+  static showDelta() {
+    View.context.textBaseline = 'top';
+    View.context.fillStyle = '#FFFFFF';
+    View.context.strokeStyle = '#FFFFFF';
+    let oldfont = View.context.font;
+    View.context.font = "bold 16px Arial"
+    View.context.fillText('Δ' + (String(Math.trunc(Main.delta*1000)).padStart(4, '0')), 5, 5);
+    View.context.font = oldfont;
   }
   static draw() {
     for (let gameObject of Game.gameObjects.values()) {
-      Transform.gameObjectToWorld(gameObject.body);      
+      Transform.gameObjectToWorld(gameObject.body);
       gameObject.body.draw();
     }
   }
@@ -43,5 +52,9 @@ export default class Main {
     for (let gameobject of Game.gameObjects.values()) {
       gameobject.body.move();
     }
+  }
+  static checkMouse() {
+    let interactionOccured = false;  //TODO: add buttons and make body parts clickable..
+    View.handleCameraDrag(interactionOccured);
   }
 }
