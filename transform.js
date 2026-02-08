e141f88f09a7a4f34cf5799b60f10ecfbd25168c
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-08 12:33:08 -0400

    seperated localToWorld from ordering of subparts.

--- transform.js
+++ transform.js
@@ -2,7 +2,7 @@ import Vec from './vec.js';
 import View from './view.js';
 
 export default class Transform {
-  static gameObjectToWorld(obj) {
+  static bodyPartsToLocal(obj) {
     let scale = View.camera.zoom;
     obj.calculatedFaces.length = 0;
     if (obj.faces) {//Bodies don't have faces, only their parts do..
@@ -16,23 +16,29 @@ export default class Transform {
       if (obj.parts.length === 0) return; // required to return from recursion..this is a this was a "leaf" node
     }
     for (let part of obj.parts) {//Process all the parts in the whole obj..
-      Transform.gameObjectToWorld(part, scale); //let children orientate themselves
+      Transform.bodyPartsToLocal(part, scale); //let children orientate themselves
       let scaledPartPosition = Vec.scale(part.position, scale)
-      for (let cf of part.calculatedFaces) { 
-        for (let p of cf.points) {          
+      for (let cf of part.calculatedFaces) {
+        for (let p of cf.points) {
           Vec.rotateInPlace(p, part.facing);
           Vec.addInPlace(p, scaledPartPosition);
         }
       }
       part.calculatedPosition = scaledPartPosition;
-      obj.calculatedFaces.push(...part.calculatedFaces); 
+      obj.calculatedFaces.push(...part.calculatedFaces);
     }
+  }
+  static localToWorld(obj) {  
+    console.log ('localtoWord obj:',obj);              
+    let world = [];
     for (let cf of obj.calculatedFaces) {
+      let wf = {color:cf.color, points:[]}
       for (let p of cf.points) {
-        Vec.rotateInPlace(p, obj.facing); 
-        Vec.addInPlace(p, obj.position); 
+        wf.points.push (Vec.add(Vec.rotation (p,obj.facing), obj.position));
       }
+      world.push (wf);
     }
+    return world;
   }
   static worldToScreen(worldCoordinate) {
     let screen = Vec.add(Vec.scale(Vec.sub(worldCoordinate, View.camera), View.camera.zoom), View.screenCenter);
