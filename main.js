3d5fbec95a13e3bc01a06cf29488d2f1f9cbf58f
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-10 03:30:09 -0400

    indenting for tattler.

--- main.js
+++ main.js
@@ -7,27 +7,39 @@ export default class Main {
   static oldTime = 0;
   static creatorsFunction = undefined;  //A callback for the user to use once per loop.
   static tickMsg = [];
+  static debugIndent =0;
+  static maxFrames =0;
+  static currentFrame = 0;
   static run(fps = 0) {
     let startTime = Date.now();
     Main.loopTime = 0;
     if (fps === 0) {
       View.bgColor = '#302';
       Main.continue = false;
+      Main.maxFrames = 0;
       Main.fpsMillis = 1 / 24;
     } else {
       View.bgColor = '#000';
       Main.continue = true;
       Main.fpsMillis = 1000 / fps;
+      Main.maxFrames = Number.MAX_VALUE;
     }
     Main.loop(startTime)
   }
-
+  static runFrames (numFrames){
+      View.bgColor = '#302';
+      Main.continue = false;
+      Main.maxFrames = numFrames;
+      Main.fpsMillis = 1 / 24;    
+  }
   static loop(startTime) {
     if (startTime) Main.oldTime = startTime;
     Main.time = Date.now();
     Main.delta = (Main.time - Main.oldTime) / 1000;;
     Main.oldTime = Main.time;
     Main.doWork();
+    Main.currentFrame ++;
+    if (Main.currentFrame> Main.maxFrames) Main.continue = false;
     if (Main.continue) setTimeout(Main.loop, Main.fpsMillis);
   }
   static doWork() {
@@ -47,11 +59,14 @@ export default class Main {
     View.context.strokeStyle = '#FFFFFF';
     let oldfont = View.context.font;
     View.context.font = "bold 16px Arial"
-    View.context.fillText('Δ' + String(Math.trunc(Main.delta * 1000)).padStart(4, '0') + ' : Σ' + String(Main.loopTime).padStart(4, '0'), 5, y);
+    View.context.fillText(String(Main.currentFrame).padStart(6, '0')+' Δ' + String(Math.trunc(Main.delta * 1000)).padStart(4, '0') + ' : Σ' + String(Main.loopTime).padStart(4, '0'), 5, y);
     
     for (let msg of Main.tickMsg){
+      this.debugIndent += msg.indent;
       y+=20;
-      View.context.fillText('↳ ' + msg, 15, y);  
+      let indStr = '|';
+      for (let i=0;i<this.debugIndent;i++) indStr +='   ';
+      View.context.fillText(indStr + msg.text, 15, y);  //↳
     }
     Main.tickMsg.length=0;
     View.context.font = oldfont;
@@ -71,6 +86,7 @@ export default class Main {
         }
         path.closePath();
         View.context.fill(path);
+        //TODO: Dot in center..
       }
     }
   }
