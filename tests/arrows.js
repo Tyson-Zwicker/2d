45d643e2357e2f1fed70023d92e81b3a9809af88
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-13 16:38:56 -0400

    shows effect of pivot points that ore off center-of-mass

--- tests/arrows.js
+++ tests/arrows.js
@@ -25,23 +25,24 @@ let arrow2 = [ //rotates around base.
   { x: 0, y: w }
 ];
 
-const greenArrow = new BodyPart('green-arrow', [{ "color": '#0f0', "points": arrow }]);
+const greenArrow = new BodyPart('green-arrow', [{ "color": '#0f0', "points": arrow }],10);
 greenArrow.ownRotation = 0;
 greenArrow.offsetPosition = { "x": 0, "y": 0 };
 
-const blueArrow = new BodyPart('blue-arrow', [{ color: '#00f', points: arrow2 }]);
-const redArrow = new BodyPart('red-arrow', [{ color: '#f00', points: arrow2 }]);
-
-greenArrow.partAdd(blueArrow, { x: l, y: 0 }, 0);
+const blueArrow = new BodyPart('blue-arrow', [{ color: '#00f', points: arrow2 }], 10);
+const redArrow = new BodyPart('red-arrow', [{ color: '#f00', points: arrow2 }], 10);
+const cyanArrow = new BodyPart('cyan-arrow', [{ color: '#0ff', points: arrow2 }], 10);
+const ship = new GameObject('arrows', greenArrow);
+greenArrow.partAdd(blueArrow, { x: l, y: 0 }, 30);
 blueArrow.partAdd(redArrow, { x: l * 2, y: 0 }, 0);
+redArrow.partAdd(cyanArrow, { x: l * 2, y: 0 }, 0);
 
-const ship = new GameObject('ship', greenArrow);
 Game.add(ship);
 
 Main.creatorsFunction = function () {
   greenArrow.ownRotation += .1;
-  blueArrow.ownRotation += 0.1;
-  redArrow.ownRotation += -.3;
+  blueArrow.ownRotation += -0.3;
+  redArrow.ownRotation += .6;
+  cyanArrow.ownRotation += -1;
 }
-
 Main.run(60);
