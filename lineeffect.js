257f99bbd91e48c1d48f8c29da214208f44490a3
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-18 18:46:40 -0400

    removed debugging code

--- lineeffect.js
+++ lineeffect.js
@@ -3,27 +3,20 @@ import Vec from './vec.js';
 import Main from './main.js';
 export default class LineEffect {
   life = undefined;
-  static nextID =0;
   constructor(startPoint, endPoint, color, thickness, durationInSeconds) {//Points should be in World Coordinates.
     this.startPoint = startPoint;
     this.endPoint = endPoint;
     this.color = color;
     this.thickness = thickness;
     this.durationInSeconds = durationInSeconds;
-    this.life = durationInSeconds;
-    this.id = LineEffect.nextID;
-    LineEffect.nextID++;
+    this.life = durationInSeconds;    
   }
   render() {
-    let debug = true;
     let screenPoint0 = Vec.add(Vec.scale(Vec.sub(this.startPoint, View.camera), View.camera.zoom), View.screenCenter);
     let screenPoint1 = Vec.add(Vec.scale(Vec.sub(this.endPoint, View.camera), View.camera.zoom), View.screenCenter);
     let opacity = Math.floor (15 * (this.life/ this.durationInSeconds));    
     let color = this.color + opacity.toString(16);
-    if (debug) {
-      console.log (`${this.id} c = ${color} | p0=${screenPoint0.x},${screenPoint0.y}| p1=${screenPoint1.x},${screenPoint1.y}`);
-    }
-
+    
     View.context.strokeStyle = color;
     View.context.lineWidth = this.thickness;
     View.context.beginPath();
@@ -32,7 +25,6 @@ export default class LineEffect {
     View.context.stroke();
     this.life -= Main.delta;
     let stillAlive = (this.life > 0 && opacity>0)
-    //if (debug &&!stillAlive) Main.continue = false;
     return stillAlive;
   }
 }
--- radialeffect.js
+++ radialeffect.js
@@ -4,7 +4,6 @@ import Main from './main.js';
 import Vec from '../vec.js';
 
 export default class RadialEffect {
-  static nextID = 0;
   constructor(point, initialRadius, expansionRate, color, thickness, durationInSeconds, startAngle, endAngle) {
     this.point = point;
     this.initialRadius = initialRadius;
@@ -22,30 +21,21 @@ export default class RadialEffect {
     } else {
       this.startAngleRadians = startAngle * (Math.PI / 180);
       this.endAngleRadians = endAngle * (Math.PI / 180);
-
     }
     this.radius = initialRadius;
-    this.id = RadialEffect.nextID;
-    RadialEffect.nextID++;
   }
   render() {
-    let debug = true;
-
     let opacity = Math.floor(15 * (this.life / this.durationInSeconds));
     let color = this.color + opacity.toString(16);
     View.context.strokeStyle = color;
     let screenPoint = Vec.add(Vec.scale(Vec.sub(this.point, View.camera), View.camera.zoom), View.screenCenter);
     let zoomedRadius = this.radius * View.camera.zoom;
-    if (debug) {
-      console.log(`${this.id} c = ${color} | p=${screenPoint.x},${screenPoint.y}| zr=${zoomedRadius}`);
-      console.log(`sa ${this.startAngleRadians}, ee=${this.endAngleRadians}`)
-    }
     View.context.lineWidth = this.thickness;
     View.context.beginPath();
     View.context.ellipse(screenPoint.x, screenPoint.y, zoomedRadius, zoomedRadius, 0, this.startAngleRadians, this.endAngleRadians);
     View.context.stroke();
     this.radius += this.expansionRate * Main.delta;
     this.life -= Main.delta;
-    return (this.life > 0);
+    return (this.life > 0 && opacity>0);
   }
 }
