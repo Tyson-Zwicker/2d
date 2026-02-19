8f0f9311f0c84cca90a62d02735929118d9f1d75
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-19 14:03:46 -0400

    faces use appearance instead of color

--- tests/move/test.js
+++ tests/move/test.js
@@ -4,11 +4,11 @@ import GameObject from '../../gameobject.js';
 import Part from '../../part.js';
 import Keyboard from '../../keyboard.js';
 import Vec from '../../vec.js';
-
+import Appearance from '../../appearance.js';
 let blockPoints = [{ x: -10, y: -10 }, { x: 10, y: -10 }, { x: 10, y: 10 }, { x: -10, y: 10 }];
-let redBlock = new Part('red-block', [{ color: "#f00", points: blockPoints }], 1);
-let greenBlock = new Part('green-block', [{ color: '#0f0', points: blockPoints }], 1);
-let blueBlock = new Part('blue-block', [{ color: '#00f', points: blockPoints }], 1);
+let redBlock = new Part('red-block', [{ appearance: Appearance.Red, points: blockPoints }], 1);
+let greenBlock = new Part('green-block', [{ appearance: Appearance.Green, points: blockPoints }], 1);
+let blueBlock = new Part('blue-block', [{ appearance: Appearance.Blue, points: blockPoints }], 1);
 let gameObject = new GameObject('blocks');
 
 redBlock.addTo(gameObject, { x: 0, y: 0 }, 0);
@@ -16,16 +16,16 @@ let gameObjects = [];
 for (let j = 0; j < 4; j++) {
   let gameObject = new GameObject('blocks' + j);
   gameObjects.push(gameObject);
-  let redBlock = new Part('red-block', [{ color: "#f00", points: blockPoints }], 1);
-  redBlock.addTo(gameObject, { x: 0, y: 0 }, 0);
+  let r = redBlock.clone();
+  r.addTo(gameObject, { x: 0, y: 0 }, 0);
   for (let i = 0; i < 10; i++) {
     let g = greenBlock.clone();
     g.name = 'green-block' + i;
     let b = blueBlock.clone();
     b.name = 'blue-block' + i;
     let sign = (i % 2 == 0) ? -1 : 1;
-    g.addTo(redBlock, { x: sign * 20 * i, y: 0 }, 0);
-    b.addTo(redBlock, { x: sign * -20 * i, y: 0 }, 0);
+    g.addTo(r, { x: sign * 20 * i, y: 0 }, 0);
+    b.addTo(r, { x: sign * -20 * i, y: 0 }, 0);
   }
   gameObject.finalize();
   Game.add(gameObject, { x: -600 + (300 * j), y: 0 }, j * 90);
@@ -34,14 +34,11 @@ Keyboard.setKeyFunction('7', () => {
   for (let i = 0;i < 4; i++) {
     let obj = Game.get('blocks'+i);
     let part = obj.getPart('green-block9');
-    let position = part.localPosition; //use part's center..
-    //Create a vector perpendicular to the body, with magnitude of desired force.
+    let position = part.localPosition; 
     let force = 20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);
-    
+    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
     obj.applyForce(forceVector, position);
-    //Main.continue = false;
   }
 });
 
@@ -49,14 +46,11 @@ Keyboard.setKeyFunction('9', () => {
   for (let i = 0;i < 4; i++) {
     let obj = Game.get('blocks'+i);
     let part = obj.getPart('green-block9');
-    let position = part.localPosition; //use part's center..
-    //Create a vector perpendicular to the body, with magnitude of desired force.
+    let position = part.localPosition;
     let force = -20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);
-    
+    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
-    obj.applyForce(forceVector, position);
-    //Main.continue = false;
+    obj.applyForce(forceVector, position);    
   }
 });
 
@@ -64,14 +58,11 @@ Keyboard.setKeyFunction('1', () => {
   for (let i = 0;i < 4; i++) {
     let obj = Game.get('blocks'+i);
     let part = obj.getPart('blue-block9');
-    let position = part.localPosition; //use part's center..
-    //Create a vector perpendicular to the body, with magnitude of desired force.
+    let position = part.localPosition;
     let force = 20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);
-    
+    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
-    obj.applyForce(forceVector, position);
-    //Main.continue = false;
+    obj.applyForce(forceVector, position);    
   }
 });
 
@@ -79,28 +70,22 @@ Keyboard.setKeyFunction('3', () => {
   for (let i = 0;i < 4; i++) {
     let obj = Game.get('blocks'+i);
     let part = obj.getPart('blue-block9');
-    let position = part.localPosition; //use part's center..
-    //Create a vector perpendicular to the body, with magnitude of desired force.
+    let position = part.localPosition; 
     let force = -20;
     let rotatedPos = Vec.rotate (position,obj.worldRotation);
-    
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
     obj.applyForce(forceVector, position);
-    //Main.continue = false;
   }
 });
 Keyboard.setKeyFunction(' ', () => {
   for (let i = 0;i < 4; i++) {
     let obj = Game.get('blocks'+i);
     let part = obj.getPart('blue-block9');
-    let position = part.localPosition; //use part's center..
-    //Create a vector perpendicular to the body, with magnitude of desired force.
+    let position = part.localPosition; 
     let force = -100;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);
-    
+    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
     let forceVector = Vec.scale(Vec.norm(rotatedPos), force);
     obj.applyForce(forceVector, position);
-    //Main.continue = false;
   }
 });
 Main.run(60);
--- tests/render/test.js
+++ tests/render/test.js
@@ -2,11 +2,12 @@ import Main from '../../main.js';
 import Game from '../../game.js';
 import GameObject from '../../gameobject.js';
 import Part from '../../part.js';
+import Appearance from '../../appearance.js';
 
 let blockPoints = [{ x: -10, y: -10 }, { x: 10, y: -10 }, { x: 10, y: 10 }, { x: -10, y: 10 }];
-let redBlock = new Part('red-block', [{ color: "#f00", points: blockPoints }], 1);
-let greenBlock = new Part('green-block', [{ color: '#0f0', points: blockPoints }], 1);
-let blueBlock = new Part('blue-block', [{ color: '#00f', points: blockPoints }], 1);
+let redBlock = new Part('red-block', [{ appearance: Appearance.Red, points: blockPoints }], 1);
+let greenBlock = new Part('green-block', [{ appearance: Appearance.Green, points: blockPoints }], 1);
+let blueBlock = new Part('blue-block', [{ appearance: Appearance.Blue, points: blockPoints }], 1);
 let gameObject = new GameObject('blocks');
 redBlock.addTo(gameObject, { x: 0, y: 0 }, 0);
 blueBlock.addTo(redBlock, { x: 20, y: 0 }, 45);
