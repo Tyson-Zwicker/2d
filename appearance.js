bfe1e8564c03360cfe0e720732ac02cf2f303fa8
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-19 14:07:22 -0400

    appearances replace simple 'color' for complex objects.  Appearances have facades like 'hovered','highlighted', 'shadowed'

---
+++ appearance.js
@@ -0,0 +1,65 @@
+import Facade from './facade.js';
+export default class Appearance {
+  normal = Facade.normal;
+  hovered = Facade.hovered;
+  pressed = Facade.pressed;
+  shadowed = Facade.shadowed;
+  highlighted = Facade.highlighted;
+  constructor(normal, hovered, pressed, shadowed, highlighted) {
+    this.normal = normal;
+    this.hovered = hovered;
+    this.pressed = pressed;
+    this.shadowed = shadowed;
+    this.highlighted = highlighted;
+  }
+  static Default = new Appearance(
+    new Facade('#444', '#ccc', '#fff', 1, 'Arial', 12),
+    new Facade('#ddd', '#ddd', '#000', 1, 'Arial', 12),
+    new Facade('#eee', '#eee', '#000', 1, 'Arial', 12),
+    new Facade('#222', '#444', '#777', 1, 'Arial', 12),
+    new Facade('#fff', '#eee', '#222', 1, 'Arial', 12)
+  );
+  static Red = new Appearance(
+    new Facade('#f00', '#f00', '#fff', 1, 'Arial', 12),
+    new Facade('#f55', '#f55', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#f66', '#000', 1, 'Arial', 12),
+    new Facade('#900', '#900', '#777', 1, 'Arial', 12),
+    new Facade('#f66', '#f66', '#222', 1, 'Arial', 12)
+  );
+  static Green = new Appearance(
+    new Facade('#0f0', '#0f0', '#fff', 1, 'Arial', 12),
+    new Facade('#5f5', '#5f5', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#6f6', '#000', 1, 'Arial', 12),
+    new Facade('#090', '#090', '#777', 1, 'Arial', 12),
+    new Facade('#6f6', '#6f6', '#222', 1, 'Arial', 12)
+  );
+  static Blue = new Appearance(
+    new Facade('#00f', '#00f', '#fff', 1, 'Arial', 12),
+    new Facade('#55f', '#55f', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#66f', '#000', 1, 'Arial', 12),
+    new Facade('#009', '#009', '#777', 1, 'Arial', 12),
+    new Facade('#66f', '#66f', '#222', 1, 'Arial', 12)
+  );  
+  static Yellow = new Appearance(
+    new Facade('#ff0', '#ff0', '#fff', 1, 'Arial', 12),
+    new Facade('#ff5', '#ff5', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#ff6', '#000', 1, 'Arial', 12),
+    new Facade('#990', '#990', '#777', 1, 'Arial', 12),
+    new Facade('#ff6', '#ff6', '#222', 1, 'Arial', 12)
+  );
+  static Cyan = new Appearance(
+    new Facade('#0ff', '#0ff', '#fff', 1, 'Arial', 12),
+    new Facade('#5ff', '#5ff', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#6ff', '#000', 1, 'Arial', 12),
+    new Facade('#099', '#099', '#777', 1, 'Arial', 12),
+    new Facade('#6ff', '#6ff', '#222', 1, 'Arial', 12)
+  );
+  static Magenta = new Appearance(
+    new Facade('#f0f', '#f0f', '#fff', 1, 'Arial', 12),
+    new Facade('#f5f', '#f5f', '#fff', 1, 'Arial', 12),
+    new Facade('#fff', '#f6f', '#000', 1, 'Arial', 12),
+    new Facade('#909', '#909', '#777', 1, 'Arial', 12),
+    new Facade('#f6f', '#f6f', '#222', 1, 'Arial', 12)
+  );
+}
+
---
+++ facade.js
@@ -0,0 +1,15 @@
+export default class Facade {
+  static normal = new Facade();
+  static hovered = new Facade('#555', '#fff', '#fff', 1, 'Arial', 12);
+  static pressed = new Facade('#fff', '#666', '#000', 1, 'Arial', 12);  
+  static shadowed = new Facade('#222', '#222', '#777', 1, 'Arial', 12);
+  static highlighted = new Facade('#666', '#444', '#222', 1, 'Arial', 12);
+  constructor(backgroundColor = '#444', borderColor = '#eee', textColor = '#ddd', borderWidth = 1, fontName = 'Arial', fontSize = 12) {
+    this.backgroundColor = backgroundColor;
+    this.borderColor = borderColor;
+    this.textColor = textColor;
+    this.borderWidth = borderWidth;
+    this.fontName = fontName;
+    this.fontSize = fontSize;
+  }
+}
