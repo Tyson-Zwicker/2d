2661c990c02e5a867ca5bbdf53458dd8d56153c6
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-18 20:05:00 -0400

    works.

--- particleeffect.js
+++ particleeffect.js
@@ -1,10 +1,11 @@
 
 import View from './view.js';
 import Rnd from './rnd.js';
+import Main from './main.js';
+import Vec from './vec.js';
 export default class ParticleEffect {
-  constructor(point, velocity, color, thickness, durationInSeconds) {
-    this.point = point;
-    this.initialRadius = initialRadius;
+  constructor(position, velocity, color, thickness, durationInSeconds) {
+    this.position = position;
     this.velocity = velocity;
     this.color = color;
     this.thickness = thickness;
@@ -12,19 +13,25 @@ export default class ParticleEffect {
     this.life = durationInSeconds;
   }
   render() {
-    let opacity = 16 * (this.durationInSeconds / this.life);
-    View.context.fillStyle = this.color + opacity.toString(16);
-    View.context.lineWidth = this.thickness;
-    let screenPoint = Vec.add(Vec.scale(Vec.sub(this.point, View.camera), View.camera.zoom), View.screenCenter);
-    context.fillRect(screenPoint.x - this.thickness / 2, screenPoint.y - this.thickness / 2, this.thickness, this.thickness);
+    let opacity = Math.floor (15 * (this.life/ this.durationInSeconds));
+    let color = this.color + opacity.toString(16);
+    View.context.fillStyle = color;
+    let screenPoint = Vec.add(Vec.scale(Vec.sub(this.position, View.camera), View.camera.zoom), View.screenCenter);    
+    View.context.fillRect(screenPoint.x - this.thickness / 2, screenPoint.y - this.thickness / 2, this.thickness, this.thickness);
     this.life -= Main.delta;
-    this.position = Vec.add(this.position, this.velocity);
-    return (this.life > 0);
+
+    this.position = Vec.add(this.position, Vec.scale(this.velocity, Main.delta));    
+    let stillAlive = (this.life > 0 && opacity>0)
+    return stillAlive;
   }
-  generateGroup(origin, angle, angleSpan, velMin, velMax, thickness, color, groupSize, durMin, durMax) {
+  static generateGroup(origin, angle, angleSpan, velMin, velMax, thickness, color, groupSize, durMin, durMax) {
     let particles = [];
     for (let i = 0; i < groupSize; i++) {
-      particles.push(new ParticleEffect(origin, Rnd.angle(angle, angleSpan), Rnd.float(velMin, velMax), color, thickness, Rnd.float(durMin, durMax)));
+      let velocity = Rnd.vec (angle-angleSpan/2, angle+angleSpan/2,velMin, velMax);
+      let duration = Rnd.float (durMin, durMax);
+      particles.push(
+        new ParticleEffect (origin, velocity, color, thickness, duration)
+      );
     }
     return particles;
   }
