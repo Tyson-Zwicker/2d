608c44b717a1d6288bf022e9d2c6aadbe2381c2a
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-19 23:40:00 -0400

    change load timing and fixed reference.

--- tests/gui/test.js
+++ tests/gui/test.js
@@ -1,11 +1,12 @@
-import Main from '../../code/main.js';
-import Appearance from '../../code/appearance.js';
-import Rnd from '../../code/rnd.js';
-import GUI from '../../code/gui.js';
+import Main from '../../main.js';
+import Appearance from '../../appearance.js';
+import GUI from '../../gui.js';
 
-document.addEventListener('DOMContentLoaded', function () {
-  makeGUI();  
-});
+
+makeGUI();
+console.log ('PRE-RUN: GUI elements:', GUI.elements.length, 'panels:', GUI.panels.size);
+Main.run();
+
 
 function makeGUI() {
   GUI.initialize(100, 30, 5, 5, Appearance.Green);
@@ -17,13 +18,13 @@ function makeGUI() {
   GUI.addButton('left', 'Click Me 2', false, (response) => { console.log(response); alert(`${response.owner} says ${response.value}`) }, 'World');
   GUI.addText('left', 'Left Text 3');
   let listOptions = [
-    {text:'Item 1', value:'A'},
-    {text:'Option 2', value:'B'},
-    {text:'Selection C', value:'C'},
-    {text:'D!', value:'D'}
+    { text: 'Item 1', value: 'A' },
+    { text: 'Option 2', value: 'B' },
+    { text: 'Selection C', value: 'C' },
+    { text: 'D!', value: 'D' }
   ]
-  GUI.addList ('left','Choose',listOptions,(newValue)=>{alert ('left = '+newValue);},'A');
-  GUI.addList ('right','Choose',listOptions,(newValue)=>{alert ('right = '+newValue);},'B');
-  GUI.addList ('top','Choose',listOptions,(newValue)=>{alert ('top = '+newValue);},'C');
-  GUI.addList ('bottom','Choose',listOptions,(newValue)=>{alert ('bottom = '+newValue);},'D');
+  GUI.addList('left', 'Choose', listOptions, (newValue) => { alert('left = ' + newValue); }, 'A');
+  GUI.addList('right', 'Choose', listOptions, (newValue) => { alert('right = ' + newValue); }, 'B');
+  GUI.addList('top', 'Choose', listOptions, (newValue) => { alert('top = ' + newValue); }, 'C');
+  GUI.addList('bottom', 'Choose', listOptions, (newValue) => { alert('bottom = ' + newValue); }, 'D');
 }
