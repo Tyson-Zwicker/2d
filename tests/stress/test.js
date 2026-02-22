ef4e2dc7b8599a83ded67de4ac4c9f4bcf07b86b
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-22 10:26:39 -0400

    stress test for quadtree - 1200 dynamic objects at 30 fps.

--- tests/stress/test.js
+++ tests/stress/test.js
@@ -9,17 +9,20 @@ let redBlock = new Part('red-block', [{ appearance: Appearance.Red, points: bloc
 let greenBlock = new Part('green-block', [{ appearance: Appearance.Green, points: blockPoints }], 1);
 let blueBlock = new Part('blue-block', [{ appearance: Appearance.Blue, points: blockPoints }], 1);
 
-for (let i = 0; i < 1000; i++) {
-  let gameObject = new GameObject('blocks');
+for (let i = 0; i < 1200; i++) {
+  let gameObject = new GameObject('blocks'+i);
   gameObject.velocity = { x: Rnd.int(-10, 10), y: Rnd.int(-10, 10) };
-  redBlock.clone().addTo(gameObject, { x: 0, y: 0 }, 0);
-  blueBlock.clone().addTo(redBlock, { x: 20, y: 0 }, 45);
-  greenBlock.clone().addTo(redBlock, { x: -20, y: 0 }, 0);
+  let r = redBlock.clone();
+  let g = greenBlock.clone();
+  let b = blueBlock.clone();
+  r.addTo(gameObject, { x: 0, y: 0 }, 0);
+  b.addTo(r, { x: 20, y: 0 }, 45);
+  g.addTo(r, { x: -20, y: 0 }, 0);
   gameObject.finalize();
   
-  Game.add(gameObject, { x: Rnd.int(-10000, 10000), y: Rnd.int(-10000, 10000) }, 0);
+  Game.add(gameObject, { x: Rnd.int(-100, 100), y: Rnd.int(-100, 100) }, 0);
   
 }
 
-Main.run();
+Main.run(30);
 
