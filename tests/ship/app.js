afd8fb6377608a106e2f5a949ae735cd7ff7f723
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-23 22:43:09 -0400

    appearance.js
    building a 'ship' to test attached particle generators for thrusters, and then collisions with a complex object.

--- tests/ship/app.js
+++ tests/ship/app.js
@@ -5,86 +5,101 @@ import Part from '../../part.js';
 import Keyboard from '../../keyboard.js';
 import Vec from '../../vec.js';
 import Appearance from '../../appearance.js';
+import Facade from '../../facade.js';
+let fname = 'Arial'
+let fsize = 12;
 
-let blockPoints = [{ x: -10, y: -10 }, { x: 10, y: -10 }, { x: 10, y: 10 }, { x: -10, y: 10 }];
-let redBlock = new Part('red-block', [{ appearance: Appearance.Red, points: blockPoints }], 1);
-let greenBlock = new Part('green-block', [{ appearance: Appearance.Green, points: blockPoints }], 1);
-let blueBlock = new Part('blue-block', [{ appearance: Appearance.Blue, points: blockPoints }], 1);
-let gameObject = new GameObject('blocks');
+let fc_hullGrey1 = new Facade('#344', '#456', '#fff', 1, fname, fsize);
+let fc_hullGrey2 = new Facade('#467', '#67a', '#fff', 11, GainNode, fsize);
 
-redBlock.addTo(gameObject, { x: 0, y: 0 }, 0);
-let gameObjects = [];
-for (let j = 0; j < 4; j++) {
-  let gameObject = new GameObject('blocks' + j);
-  gameObjects.push(gameObject);
-  let r = redBlock.clone();
-  r.addTo(gameObject, { x: 0, y: 0 }, 0);
-  for (let i = 0; i < 10; i++) {
-    let g = greenBlock.clone();
-    g.name = 'green-block' + i;
-    let b = blueBlock.clone();
-    b.name = 'blue-block' + i;
-    let sign = (i % 2 == 0) ? -1 : 1;
-    g.addTo(r, { x: sign * 20 * i, y: 0 }, 0);
-    b.addTo(r, { x: sign * -20 * i, y: 0 }, 0);
-  }
-  gameObject.finalize();
-  Game.add(gameObject, { x: -600 + (300 * j), y: 0 }, j * 90);
+let ap_hull1 = new Appearance(fc_hullGrey1);
+let ap_hull2 = new Appearance(fc_hullGrey2);
+
+let secSize = 10;
+let secMass = 1;
+let midMass = 10;
+let ship = new GameObject('ship', true);
+
+let pts_hull = [{ x: -secSize, y: -secSize }, { x: secSize, y: -secSize }, { x: secSize, y: secSize }, { x: -secSize, y: secSize }];
+let pts_hull_stb = [{ x: -secSize, y: secSize * .7 }, { x: secSize, y: secSize * .7 }, { x: secSize, y: secSize }, { x: -secSize, y: secSize }];
+let pts_hull_prt = [{ x: -secSize, y: -secSize }, { x: secSize, y: -secSize }, { x: secSize, y: -secSize * .7 }, { x: -secSize, y: -secSize * .7 }];
+let pts_mid = [
+  { x: -secSize * 3, y: -secSize }, { x: -secSize * .5, y: -secSize * 2 }, { x: secSize * .5, y: -secSize * 2 }, { x: secSize * 3, y: -secSize },
+  , { x: secSize * 3, y: secSize }, { x: secSize * .5, y: secSize * 2 }, { x: -secSize * .5, y: secSize * 2 }, { x: -secSize * 3, y: secSize }
+];
+//"Face" = {appearance: app, points:[...points...]}
+let face_mid = { appearance: ap_hull1, points: pts_mid };
+let part_mid = new Part('mid-section', [face_mid], midMass);
+part_mid.addTo(ship, { x: 0, y: 0 }, 0);
+
+let face_hull = { appearance: ap_hull1, points: pts_hull };
+let face_hull_stb = { appearance: ap_hull2, points: pts_hull_stb };
+let face_hull_prt = { appearance: app_hull2, points: pts_hull_prt };
+let fwdSections = 10;
+
+for (let i = 0; i < hullSections; i++) {
+  let part_hull = new Part('hull-center${i}', [face_hull], secMass);
+  part_hull.addTo(part_mid, { x: (secSize * 3 + secSize / 2) + (i * secSize), y: 0 }, 0);
+  //TODO: define stb and prt parts and add them at 90 and -90 rotation offset x:0, y:secSize/(+/-)2
+  //these parts also have 2 faces: hull and either stb_
 }
+
+Game.add(ship, { x: 0, y: 0 }, 0);
+
 Keyboard.setKeyFunction('7', () => {
-  for (let i = 0;i < 4; i++) {
-    let obj = Game.get('blocks'+i);
+  for (let i = 0; i < 4; i++) {
+    let obj = Game.get('blocks' + i);
     let part = obj.getPart('green-block9');
-    let position = part.localPosition; 
+    let position = part.localPosition;
     let force = 20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
+    let rotatedPos = Vec.rotate(position, obj.worldRotation);
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
     obj.applyForce(forceVector, position);
   }
 });
 
 Keyboard.setKeyFunction('9', () => {
-  for (let i = 0;i < 4; i++) {
-    let obj = Game.get('blocks'+i);
+  for (let i = 0; i < 4; i++) {
+    let obj = Game.get('blocks' + i);
     let part = obj.getPart('green-block9');
     let position = part.localPosition;
     let force = -20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
+    let rotatedPos = Vec.rotate(position, obj.worldRotation);
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
-    obj.applyForce(forceVector, position);    
+    obj.applyForce(forceVector, position);
   }
 });
 
 Keyboard.setKeyFunction('1', () => {
-  for (let i = 0;i < 4; i++) {
-    let obj = Game.get('blocks'+i);
+  for (let i = 0; i < 4; i++) {
+    let obj = Game.get('blocks' + i);
     let part = obj.getPart('blue-block9');
     let position = part.localPosition;
     let force = 20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
+    let rotatedPos = Vec.rotate(position, obj.worldRotation);
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
-    obj.applyForce(forceVector, position);    
+    obj.applyForce(forceVector, position);
   }
 });
 
 Keyboard.setKeyFunction('3', () => {
-  for (let i = 0;i < 4; i++) {
-    let obj = Game.get('blocks'+i);
+  for (let i = 0; i < 4; i++) {
+    let obj = Game.get('blocks' + i);
     let part = obj.getPart('blue-block9');
-    let position = part.localPosition; 
+    let position = part.localPosition;
     let force = -20;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);
+    let rotatedPos = Vec.rotate(position, obj.worldRotation);
     let forceVector = Vec.scale(Vec.perp(Vec.norm(rotatedPos)), force);
     obj.applyForce(forceVector, position);
   }
 });
 Keyboard.setKeyFunction(' ', () => {
-  for (let i = 0;i < 4; i++) {
-    let obj = Game.get('blocks'+i);
+  for (let i = 0; i < 4; i++) {
+    let obj = Game.get('blocks' + i);
     let part = obj.getPart('blue-block9');
-    let position = part.localPosition; 
+    let position = part.localPosition;
     let force = -100;
-    let rotatedPos = Vec.rotate (position,obj.worldRotation);    
+    let rotatedPos = Vec.rotate(position, obj.worldRotation);
     let forceVector = Vec.scale(Vec.norm(rotatedPos), force);
     obj.applyForce(forceVector, position);
   }
