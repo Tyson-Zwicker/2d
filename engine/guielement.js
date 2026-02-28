7408543fcbcf834f21bf4e2c439f9be36fd146d9
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-27 23:40:39 -0400

    Drives events to event system and uses Mien for appearance

---
+++ engine/guielement.js
@@ -0,0 +1,13 @@
+
+import GUI from './gui.js';
+
+export default class GUIElement {
+  constructor (text){    
+    this.bounds =  {x0:0, y0:0, x1:GUI.columnWidth, y1:GUI.rowHeight}; 
+    this.active = true;
+    this.text = text;
+    this.drawnBounds = undefined;
+    //This is enough for the textbox, other Element Types (button and list)
+    //require additional properties assigned in the get control methods..        
+  }
+}
