08eaa22997ff1ca9592364869ac65b2a3b081b7f
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-02-27 23:32:30 -0400

    works for gameobjects or guielements..

---
+++ engine/button.js
@@ -0,0 +1,85 @@
+import Events from './events.js';
+import GameObject from './gameobject.js';
+import GUI from './gui.js';
+import View from './view.js';
+export default class Button {
+  guiElement = undefined;
+  gameObjectPart = undefined;
+  clicked = false;
+  clickFn = undefined;
+  hovered = false;
+  pressed = false;
+  toggle = false;
+  //Must be bound by an actor or Element to do anything..
+  //They must bind the actor OR guiElement property.
+  constructor(value, toggle = false, clickFn) {
+    this.clickFn = clickFn;
+    this.toggle = toggle;
+    this.value = value;
+  }
+  checkForMouse() {
+    if (this.guiElement) {
+      let insideBounds = GUI.isMouseIn(this.guiElement);
+      return this.#doButton(insideBounds);  // Do this even if NOT in bounds to allow others to de-hover..
+    }
+    if (this.gameObjectPart) {
+     // console.log('checking button for part', this.gameObjectPart.name);
+      let mouseWorldCoords = View.screenToWorld(View.mouse.x, View.mouse.y);
+     // console.log('checking mouse in part', this.gameObjectPart.name, 'mouse coords', mouseWorldCoords);
+      let insideBounds = GameObject.isMouseIn(this.gameObjectPart);
+      return this.#doButton(insideBounds);
+    }
+  }
+  #doButton(insideBounds) {
+    let interaction = false;
+    if (insideBounds) {
+
+      if (View.mouse.buttonDown && !this.hovered) {
+        //*Must* be checked first.. mouse went down somewhere else, but not here.. doesn't affect this actor..
+        return false;
+      }
+      else if (!View.mouse.buttonDown && !this.hovered) {
+        //mouse hovers over actor, not button pressed..
+        this.hovered = true;
+        interaction = true;
+      }
+      else if (View.mouse.buttonDown && this.hovered && !this.pressed) {
+        //they just pressed on this button, which was being hovered over..
+        this.pressed = true;
+        interaction = true;
+      }
+      else if (!View.mouse.buttonDown && this.pressed) {
+        //they just let up on the button after pressing.. that is a click.
+        this.#click();
+        Events.add('button-clicked', (this.guiElement ? this.guiElement : this.GameObject));
+        this.hovered = false;
+        this.pressed = false;
+        interaction = true;
+      } else {
+        interaction = true;
+      }
+    } else {
+      this.pressed = false; // So.. it can't be clicked if its not on it..
+      this.hovered = false;
+    }
+    return interaction;
+  }
+  #click() {
+    let buttonOwner = this.guiElement;
+    let r = { "owner": buttonOwner, "value": this.value };
+    if (!this.toggle) {
+      if (typeof this.clickFn === 'function') {
+        this.clickFn(r);
+        if (this.originalFn) this.originalFn(r);
+      }
+    } else {
+      this.clicked = !this.clicked;
+      if (this.clicked) {
+        if (typeof this.clickFn === 'function') {
+          this.clickFn(r);//check for 'original function' too.
+          if (this.originalFn) this.originalFn(r);
+        }
+      }
+    }
+  }
+}
