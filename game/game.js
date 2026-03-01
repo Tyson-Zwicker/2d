e2554d57a0e277ea3e1fc99fe6b13e53165db24c
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-01 19:32:50 -0400

    gui changes

--- game/game.js
+++ game/game.js
@@ -8,25 +8,38 @@ import Part from '../engine/part.js';
 import Polygon from '../engine/polygon.js';
 import Mien from '../engine/mien.js';
 
+let starMien = new Mien('star-yellow');
+starMien.setColors ('#882','#ff0','#fff','normal');
+starMien.setColors ('#ff0','#ff6','#000','hovered');
+starMien.setColors ('#f00','#ff0','#fff','pressed'); 
 let homeStar = new GameObject('Home', true);
-let star = new Part('Sol-0', Polygon.regular(11, 150, Mien.Yellow));
-let b = new Button('star-Home', false, () => { console.log('star clicked'); });
-b.gameObjectPart = star;
-star.button = b;
+
+let star = new Part('Sol-0', Polygon.regular(11, 150, starMien).translate (700,0));
+new Button('star-Home', false, (response) => { alert('star clicked'); }).bind (star);
 
 let planet1 = new Part('Sol-1', Polygon.regular(11, 10, Mien.Red));
 planet1.spin = 10;
 star.addTo(homeStar, { x: 0, y: 0 });
-planet1.addTo(star, { x: 700, y: 0 });
+planet1.addTo(star, { x: 0, y: 0 });
 homeStar.finalize();
+
 GUI.addText('left', 'Left Text 2');
-GUI.addButton('left', 'Click Me', false, (response) => { console.log(response); alert(`${response.owner} says ${response.value}`) }, 'Hello');
+GUI.addButton('left', 'Button', false, (response) => { alert(`${response.owner} says ${response.value}`) }, 'Hello');
+GUI.addList ('left',
+   'List', 
+   [{ text: 'Option 1', value: 1 },
+    { text: 'Option 2', value: 2 },
+    { text: 'Option 3', value: 3 },]
+  ,(response)=>{alert (`${response.owner} says ${response.value}`)},2);
 Game.add(homeStar, { x: 500, y: 500 });
+
 Main.creatorsFunction = () => {
-  let events = Events.get();
+  let events = Events.getEvents();
   if (events.length > 0) {
     console.log ('Events:');  
-    console.log(Events.get());
+    for (let e of events) {
+      console.log (`${e.type} at ${e.when} from ${e.origin}`);
+    }
   }
 };
 
