bc24f648eb707ed2ff7fad5381e30327afbe5334
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-06 00:11:54 -0400

    the one index points at.

--- game/the_game.js
+++ game/the_game.js
@@ -3,6 +3,7 @@ import Button from '../engine/button.js';
 import Events from '../engine/events.js';
 import Game from '../engine/game.js';
 import GameObject from '../engine/gameobject.js';
+import GUI from '../engine/gui.js';
 import GUIElement from '../engine/guielement.js'
 import GUIPanel from '../engine/guipanel.js';
 import Part from '../engine/part.js';
@@ -12,10 +13,16 @@ import Mien from '../engine/mien.js';
 import StarSystem from '../rules/starsystem.js';
 
 
-StarSystem.Starter();
+const starterStar = StarSystem.Starter();
+let infoPanelConstraint = {
+  min: { width: 50, height: 50 }, max: { width: 250, height: 100 }
+} 
+let planetInfoPanel = new GUIPanel(undefined, 'horizontal', infoPanelConstraint);
+let planetInfoElement = GUIElement.addText (planetInfoPanel,'zones:4 population:7');
+planetInfoPanel.anchor = starterStar.gameObject;
 
 Main.creatorsFunction = () => {
-  
+  starterStar.interface.animate();
 }
 Main.run(100);
 
