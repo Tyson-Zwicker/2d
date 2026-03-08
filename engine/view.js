53bffd5ea4be9e334485297a1d8aa49fe8307492
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-08 12:22:57 -0300

    minor..

--- engine/view.js
+++ engine/view.js
@@ -10,7 +10,7 @@ export default class View {
   static mouse = { x: 0, y: 0, buttonDown: false }; //Screen coordinates
   static screenCenter = undefined;
   static zoomFactor = 10; //How much each zoom changes when the wheel is scrolled.
-
+  
   static initialize() {
     View.canvas = document.createElement('canvas');
     View.canvas.style.margin = 0;
