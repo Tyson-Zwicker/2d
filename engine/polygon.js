016c6b6c238755fe8bd3dad4d464045dc5179168
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-03-09 17:56:40 -0300

    fixed bug in transformations

--- engine/polygon.js
+++ engine/polygon.js
@@ -6,30 +6,37 @@ export default class Polygon {
     this.points = points;
     this.mien = mien;
   }
+  static translate(polygon, vector) {
+    let pts = [];
+    for (let p of polygon.points) {
+      pts.push(Vec.add (vector,p));
+    }
+    return new Polygon(pts, polygon.mien);
+  }
   static flipVertical(polygon) {
     let pts = [];
-    for (let p in polygon.points) {
+    for (let p of polygon.points) {
       pts.push({ x: p.x, y: p.y * -1 });
     }
     return new Polygon(pts, polygon.mien);
   }
   static flipHorizontal(polygon) {
     let pts = [];
-    for (let p in polygon.points) {
+    for (let p of polygon.points) {
       pts.push({ x: p.x * -1, y: p.y });
     }
     return new Polygon(pts, polygon.mien);
   }
   static scale(polygon, scale) {
     let pts = [];
-    for (let p in polygon.points) {
+    for (let p of polygon.points) {
       pts.push(Vec.scale(p, scale));
     }
     return new Polygon(pts, polygon.mien);
   }
   static rotate(polygon, angle) {
     let pts = [];
-    for (let p in polygon.points) {
+    for (let p of polygon.points) {
       pts.push(Vec.rotate(p, angle));
     }
     return new Polygon(pts, polygon.mien);
