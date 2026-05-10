ef140073aa95a7b80fab8132a853200695b214df
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-05-09 21:03:23 -0300

    tests

--- test/4_button&pan.js
+++ test/4_button&pan.js
@@ -1,4 +1,3 @@
-
 import { Main } from '../dist/main.js';
 import { Sim } from '../dist/sim.js';
 import { SimObject, Part } from '../dist/simobject.js';
@@ -6,28 +5,18 @@ import { Polygon } from '../dist/polygon.js';
 import { Mien } from '../dist/mien.js';
 import { Button } from '../dist/button.js';
 import { Camera } from '../dist/view.js';
+import { Rnd } from '../dist/rnd.js';
 
-let greenHex = new SimObject('green', 'never');
-new Part('green-part',Polygon.regular (6,20,Mien.Green)).addTo(greenHex, { x: 0, y: 0 }, 0);
-greenHex.finalize();
-Sim.add(greenHex, { x:-800, y: -800 }, 0);
-
-let blueHex = new SimObject('blue', 'never');
-new Part('blue-part',Polygon.regular (6,20,Mien.Blue)).addTo(blueHex, { x: 0, y: 0 }, 0);
-blueHex.finalize();
-Sim.add(blueHex, { x:800, y: 800 }, 0);
-
-new Button ('blue','false',blueHex,(data)=>  {
-  console.log ('click',data.owner)
-  Camera.panTo (blueHex.worldPosition.x, blueHex.worldPosition.y,1);
-});
-new Button ('green','false',greenHex, (data)=>{
-  console.log ('click',data.owner)
-  Camera.panTo (greenHex.worldPosition.x, greenHex.worldPosition.y,1);
-});
-//YOU do not add buttons to things, they get their owner when constructed...
-//returns data = { origin: 'unknown', owner: undefined, type: 'click', value: this.value };
-
-//Two objects.. clicking on them causes camera to "Pan" and lock on them..
-
+let mapSize = { x: 1000, y: 1000 };
+for (let i = 0; i < 40; i++) {
+  let obj = new SimObject(`${i}`, 'never');
+  new Part(`${i}`, Polygon.regular(5, 50, Mien.Yellow)).addTo(obj, { x: 0, y: 0 }, 0);
+  
+  new Button(`${i}`, false, obj, (data) => {
+    let pos = Sim.get(data.owner.name).worldPosition;
+    Camera.panTo(pos.x, pos.y, 1);
+  });
+  obj.finalize();
+  Sim.add(obj, { x: Math.random() * mapSize.x - mapSize.x / 2, y: Math.random() * mapSize.y - mapSize.y / 2 });
+}
 Main.run(60);
